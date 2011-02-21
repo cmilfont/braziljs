cde5bd2fd57e9504476b145136ad813f9e499016
Christiano Milfont <cmilfont@gmail.com>
2011-02-21 19:42:41 -0300

    versao 0.0.1 fechada, vou subir

--- js/index.js
+++ js/index.js
@@ -17,40 +17,32 @@ var twitterWidget;
 var twitter = function() {
 
   twitterWidget = new TWTR.Widget({
-    id: "twtr-widget",
-     version: 2,
-     type: 'search',
-     search: 'braziljs',
-     interval: 6000,
-     title: 'BrazilJS',
-     subject: 'The Brazilian JS Conference',
-     width: 'auto',
-     height: 415,
+     id: "twtr-widget", width: 'auto', height: 415,
+     version: 2, type: 'search',search: 'braziljs', interval: 6000,
+     title: 'BrazilJS', subject: 'The Brazilian JS Conference',
      theme: {
-       shell: {
-         background: '#450d0b',
-         color: '#ffffff'
-       },
-       tweets: {
-         background: '#ffffff',
-         color: '#444444',
-         links: '#1985b5'
-       }
+       shell: { background: '#450d0b', color: '#ffffff' },
+       tweets: { background: '#ffffff', color: '#444444', links: '#1985b5'}
      },
      features: {
-       scrollbar: true,
-       loop: true,
-       live: true,
-       hashtags: true,
-       timestamp: true,
-       avatars: true,
-       toptweets: true,
+       scrollbar: true,loop: true,live: true,
+       hashtags: true,timestamp: true,avatars: true,toptweets: true,
        behavior: 'default'
      }
  })
  twitterWidget.render().start();
 
 };
+
+var renderPage = function(destaque, route, callback) {
+  mostrarDestaque(destaque);
+  ativarMenu(route);
+  cb.call(this);
+};
+var iniciarTwitter = function(){
+  if(twitterWidget) { twitterWidget.start(); } else { twitter(); }
+};
+var pararTwitter = function() { if(twitterWidget)  twitterWidget.stop(); };
  
  ;(function($) {
 
@@ -59,37 +51,43 @@ var twitter = function() {
     //this.use(Sammy.XTemplate);
     
     this.get('#!/home', function() {
-    	
       this.partial('/views/home.html').then(function() {
-	      mostrarDestaque(true);
-	      ativarMenu("#!/home");
-        if(twitterWidget) {
-        	twitterWidget.start();
-        } else {
-        	twitter();
-        }
-        
-      });
-      
-    });
-    
-    this.get('#!/inscricoes', function() {
-      this.partial('/views/inscricoes.html').then(function() {
-        mostrarDestaque(false);
-        ativarMenu("#!/inscricoes");
-        if(twitterWidget)  twitterWidget.stop();
+      	renderPage(true, "#!/home", iniciarTwitter);
       });
     });
     
     this.get('#!/agenda', function() {
       this.partial('/views/agenda.html').then(function() {
-        mostrarDestaque(false);
-      	ativarMenu("#!/agenda");
-        if(twitterWidget)  twitterWidget.stop();
+      	renderPage(false, "#!/agenda", pararTwitter);
+      });
+    });
+
+    this.get('#!/local', function() {
+      this.partial('/views/local.html').then(function() {
+      	renderPage(false, "#!/local", pararTwitter);
       });
     });
     
+    this.get('#!/inscricoes', function() {
+      this.partial('/views/inscricoes.html').then(function() {
+        renderPage(false, "#!/inscricoes", pararTwitter);
+      });
+    });
 
+    this.get('#!/quemsomos', function() {
+      this.partial('/views/quemsomos.html').then(function() {
+        renderPage(false, "#!/quemsomos", pararTwitter);
+      });
+    });
+
+    this.get('#!/contato', function() {
+      this.partial('/views/contato.html').then(function() {
+        renderPage(false, "#!/contato", pararTwitter);
+      });
+    });
+    
+    
+    
   });
 
   $(function() {
